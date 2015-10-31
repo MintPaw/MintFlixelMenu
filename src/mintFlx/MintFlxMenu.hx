88bfaa647c05638902e68d30f8fdd3d9bb108aa9
MintPaw <jerusanders@gmail.com>
2015-10-31 14:07:09 -0600

    Created buttons

--- src/mintFlx/MintFlxMenu.hx
+++ src/mintFlx/MintFlxMenu.hx
@@ -4,19 +4,33 @@ import flixel.FlxState;
 import flixel.FlxSprite;
 import flixel.FlxG;
 import flixel.text.FlxText;
+import flixel.ui.FlxButton;
 
 class MintFlxMenu extends FlxState
 {
 	public var titleString:String = "Unnamed";
 	public var subtitleString:String = "Unnamed";
+	public var playButtonString:String = "Play";
+	public var settingsButtonString:String = "Settings";
+	public var creditsButtonString:String = "Credits";
 
 	public var titleGraphicPath:String = null;
 	public var subtitleGraphicPath:String = null;
 
+	public var playCallback:Void->Void;
+
+	public var fadeTime:Float = 2;
+	public var debugFadeTime:Float = .25;
+
 	private var _currentMenu:String;
+
+	private var _fadeTime:Float;
 	
 	private var _title:FlxSprite;
 	private var _subtitle:FlxSprite;
+	private var _play:FlxButton;
+	private var _settings:FlxButton;
+	private var _credits:FlxButton;
 
 	public function new()
 	{
@@ -27,9 +41,15 @@ class MintFlxMenu extends FlxState
 	{
 		super.create();
 
+		_fadeTime = fadeTime;
+
+#if debug
+		_fadeTime = debugFadeTime;
+#end
+
 		loadMenu("main");
 	}
-
+	
 	private function loadMenu(menu:String):Void
 	{
 		_currentMenu = menu;
@@ -50,9 +70,39 @@ class MintFlxMenu extends FlxState
 				cast(_subtitle, FlxText).alignment = "center";
 				_subtitle.y = _title.y + _title.height + 10;
 			}
+
+			_play = new FlxButton(
+					0, 0,
+				 	playButtonString,
+				 	internalPlayCallback);
+
+			_settings = new FlxButton(
+					0, 0,
+				 	settingsButtonString,
+				 	internalSettingsCallback);
+
+			_credits = new FlxButton(
+					0, 0,
+				 	settingsButtonString,
+				 	internalCreditsCallback);
 						
 			add(_title);
 			add(_subtitle);
 		}
+	}
+
+	private function internalPlayCallback():Void
+	{
+		FlxG.camera.fade(0x000000, _fadeTime, false, playCallback, true);
+	}
+
+	private function internalSettingsCallback():Void
+	{
+		// FlxG.camera.fade(0x000000, _fadeTime, false, playCallback, true);
+	}
+
+	private function internalCreditsCallback():Void
+	{
+		// FlxG.camera.fade(0x000000, _fadeTime, false, playCallback, true);
 	}
 }
